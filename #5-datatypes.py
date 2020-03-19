e9350d7f860c471a39324b4004e4a5eb32b01cc0
Souvik Mandal <58888934+souvik362@users.noreply.github.com>
2020-03-19 09:50:10 +0530

    Datatypes in python

---
+++ #5-datatypes.py
@@ -0,0 +1,29 @@
+# Text type - string
+x = "souvik"
+
+# Number type - integer / float / complex
+x = 32
+x = 32.2 
+x = 1j
+
+# sequence type - list / tuple /range
+x = ["apple", "Banana", "cherry"]
+x = ("apple", "Banana", "cherry")
+x = range(6)
+
+# Mapping type
+x = {"name" : "souvik", "age" : 17}
+
+# set type - set / frozen set
+x = {"apple", "banana", "orange"}
+x = frozenset({"apple", "banana", "orange"})
+
+# boolean type 
+isSheGetTheBall = True
+
+#binary types - bytes / bytearray / memoryview
+x = b"hello"
+x = bytearray(5) 
+x = memoryview(bytearray(5))
+
+print(x)
