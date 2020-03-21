ad6e30b2f5328533a62e0c8da7bf2f8c50c9f806
Souvik Mandal <58888934+souvik362@users.noreply.github.com>
2020-03-21 19:25:33 +0530

    Booleans in Python

---
+++ #8-booleans.py
@@ -0,0 +1,11 @@
+# booleans represents true or false condition
+
+a = "creaets"
+b = 203
+
+# constructing any element into boolean by bool() constructor
+myboolValue = bool(a)
+
+# function can return boolean
+def myBool():
+    return True
