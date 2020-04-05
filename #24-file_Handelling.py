7227d60d8e882efd7c842b690f90ec39fe39b1eb
Souvik Mandal <58888934+souvik362@users.noreply.github.com>
2020-04-05 12:07:14 +0530

    demofile

---
+++ #24-file_Handelling.py
@@ -0,0 +1,7 @@
+# opening file
+getfile = open('demo.txt', 'r')
+# reading file
+readFile = getfile.read()  # reding the first 5 character
+
+# closing file
+getfile.close()  # you should always close the file
