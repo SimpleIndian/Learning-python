50f062c608d74970ae91de5e36f4526a6d23af41
Souvik Mandal <58888934+souvik362@users.noreply.github.com>
2020-04-06 09:18:02 +0530

    File handelling in Python

--- #24-file_Handelling.py
+++ #24-file_Handelling.py
@@ -1,7 +1,25 @@
-# opening file
-getfile = open('demo.txt', 'r')
+import os  # for delet
+# this will methods will create the file if does not exits.
+getNewFile = open('demo.txt', 'a')  # opening file in append mode(a).
+# getNewFile = open('demo.txt', 'w')# opening file in write mode(w) (override every thing)
+# getNewFile = open('demo.txt', 'x')# opening file in create(x) mode (override every thing)
+
+# writing file
+getNewFile.write('Adding new strings....')
+getNewFile.close()  # you should always close the file after
+
+getNewFile = open('demo.txt', 'r')  # open file in reading mode
+
 # reading file
-readFile = getfile.read()  # reding the first 5 character
+readFile = getNewFile.read()  # reding the first 5 character
+print(readFile)
 
 # closing file
-getfile.close()  # you should always close the file
+getNewFile.close()
+
+# deleting the file
+if os.path.exists('demo.txt'):
+    os.remove('demo.txt')  # this will delet the file
+    print("file deleted")
+else:
+    print("file doesnot exits")
